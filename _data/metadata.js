18c853ea2c7e8bd56734484b8ebe9a2ca4208a59
joey <jmackle@ufl.edu>
2025-01-17 10:14:59 -0500

    ghp3

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
 	title: "joe mackle's blog",
-	url: "https://joemackle.github.io",
+	url: "https://joemackle.github.io/portfolio",
 	language: "en",
 	description: "a blog about everything joe mackle",
 	author: {
 		name: "Joe Mackle",
 		email: "jmackle@ufl.edu",
-		url: "https://joemackle.github.io/about/"
+		url: "https://joemackle.github.io/portfolio/about/"
 	}
 }
